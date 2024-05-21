b1d678099f227569cb36d801d5f9214d4d081aa0
Brayan Velez <brayanvelez.556@gmail.com>
2024-05-20 20:24:36 -0500

    Se creán botones y se configura navegacion hacia atras de las activitys create, form y register

--- app/src/main/java/com/example/planeapp/RegisterActivity.kt
+++ app/src/main/java/com/example/planeapp/RegisterActivity.kt
@@ -1,14 +1,17 @@
 package com.example.planeapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class RegisterActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,6 +20,12 @@ class RegisterActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnMain: Button = findViewById(R.id.button)
+        btnMain.setOnClickListener {
+            val intent: Intent = Intent(this, IntroActivity::class.java)
+            startActivity(intent)
         }
 
 
